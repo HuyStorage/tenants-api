0e21e2f69012678f08975aaa67b3bf6599637d78
huypham <huyphuocbinh204@gmail.com>
2025-06-23 14:20:41 +0700

    fix kind for user

--- source/src/main/java/com/tenant/api/constant/BaseConstant.java
+++ source/src/main/java/com/tenant/api/constant/BaseConstant.java
@@ -5,8 +5,8 @@ public class BaseConstant {
     public static final Integer USER_KIND_ADMIN = 1;
     public static final Integer USER_KIND_MANAGER = 2;
     public static final Integer USER_KIND_EMPLOYEE = 3;
-    public static final Integer USER_KIND_USER = 1000;
-    public static final Integer USER_KIND_USER_VIP = 1001;
+    public static final Integer USER_KIND_USER = 10;
+    public static final Integer USER_KIND_USER_VIP = 11;
 
     public static final Integer STATUS_ACTIVE = 1;
     public static final Integer STATUS_PENDING = 0;
